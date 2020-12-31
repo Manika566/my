017e7d38e828af18914dc6d2da291dc79dbb0e78
Priyoraj <46069506+Priyoraj@users.noreply.github.com>
2020-12-31 16:33:05 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -1,60 +1,41 @@
-$(document).ready(function () {
+var el = x => document.getElementById(x);
 
-	function readURL(input) {
-		if (input.files && input.files[0]) {
-			var reader = new FileReader();
+function showPicker() {
+  el("file-input").click();
+}
 
-			reader.onload = function (e) {
-				$('#selected_image')
-					.attr('src', e.target.result)
-					.width(176)
-					.height(176);
-			}
-			reader.readAsDataURL(input.files[0]);
-		}
-	}
+function showPicked(input) {
+  el("upload-label").innerHTML = input.files[0].name;
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    el("image-picked").src = e.target.result;
+    el("image-picked").className = "";
+  };
+  reader.readAsDataURL(input.files[0]);
+}
 
-	$('#imagefile').change(function () {
-		readURL(this);
-	});
+function analyze() {
+  var uploadFiles = el("file-input").files;
+  if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
+  el("analyze-button").innerHTML = "Analyzing...";
+  var xhr = new XMLHttpRequest();
+  var loc = window.location;
+  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true);
+  xhr.onerror = function() {
+    alert(xhr.responseText);
+  };
+  xhr.onload = function(e) {
+    if (this.readyState === 4) {
+      var response = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]}`;
+    }
+    el("analyze-button").innerHTML = "Analyze";
+  };
 
-	$("form#analysis-form").submit(function (event) {
-		event.preventDefault();
+  var fileData = new FormData();
+  fileData.append("file", uploadFiles[0]);
+  xhr.send(fileData);
+}
 
-		var analyze_button = $("button#analyze-button");
-		var imagefile = $('#imagefile')[0].files;
-
-		if (!imagefile.length > 0) {
-			alert("Please select a file to analyze!");
-		}
-		else {
-			analyze_button.html("Analyzing..");
-			analyze_button.prop("disabled", "true");
-
-			var fd = new FormData();
-			fd.append('file', imagefile[0]);
-
-			var loc = window.location;
-
-			$.ajax({
-				method: 'POST',
-				async: true,
-				url: loc.protocol + '//' + loc.hostname + ':' + loc.port + '/analyze',
-				data: fd,
-				processData: false,
-				contentType: false,
-			}).done(function (data) {
-				console.log("Done Request!");
-				$("#result").html("Result= "+ data.result);
-			}).fail(function (e) {
-				console.log("Fail Request!");
-				console.log(e);
-			});
-		};
-
-		analyze_button.prop("disabled", "");
-		analyze_button.html("Analyze");
-		console.log("Submitted!");
-	});
-});
